b3af44311e3bb410da2fe30ac8656159bf8e1e6f
Masataro Asai <guicho2.71828@gmail.com>
2016-12-19 13:58:45 +0900

    found a bug!!

--- learn.py
+++ learn.py
@@ -132,30 +132,28 @@ def aae_train (name, epoch=128,computational_effort_factor=8,noise=False):
                 x_batch = x_train[i*batch_size:(i+1)*batch_size]
                 real_batch = real_train[i*batch_size:(i+1)*batch_size]
                 fake_batch = fake_train[i*batch_size:(i+1)*batch_size]
+                d_batch = np.concatenate((fake_batch,real_batch),1)
+                g_batch = np.concatenate((real_batch,real_batch),1)
                 set_trainable(encoder, True)
                 set_trainable(decoder, True)
                 map(lambda d:set_trainable(d,False), discriminators)
                 r_loss = aae_r.train_on_batch(x_batch, x_batch)
                 def test():
                     return \
-                        aae_d.test_on_batch( \
-                            x_batch, np.concatenate((real_batch,fake_batch),1)), \
-                        aae_g.test_on_batch( \
-                            x_batch, np.concatenate((real_batch,real_batch),1))
+                        aae_d.test_on_batch(x_batch, d_batch), \
+                        aae_g.test_on_batch(x_batch, g_batch)
                 def train_discriminator():
                     d['discriminator'] += 1
                     set_trainable(encoder, False)
                     set_trainable(decoder, False)
                     map(lambda d:set_trainable(d,True), discriminators)
-                    return aae_d.train_on_batch(
-                        x_batch, np.concatenate((fake_batch,real_batch),1))
+                    return aae_d.train_on_batch(x_batch, d_batch)
                 def train_generator():
                     d['generator'] += 1
                     set_trainable(encoder, True)
                     set_trainable(decoder, False)
                     map(lambda d:set_trainable(d,False), discriminators)
-                    return aae_g.train_on_batch(
-                        x_batch, np.concatenate((real_batch,real_batch),1))
+                    return aae_g.train_on_batch(x_batch, g_batch)
                 for j in range(1):
                     train_discriminator()
                     train_generator()
